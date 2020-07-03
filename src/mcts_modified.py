97f8acf6939a6a53326bdf4bfdcead90f781fbfa
dtnguyen277 <dtnguyen277@users.noreply.github.com>
2020-07-02 23:53:14 -0700

    exp1
    analysis

--- src/mcts_modified.py
+++ src/mcts_modified.py
@@ -2,7 +2,7 @@ from mcts_node import MCTSNode
 from random import choice
 from math import sqrt, log
 
-num_nodes = 25 #1000
+num_nodes = 50 #1000
 explore_factor = 2
 
 # helper function for traverse_nodes()
